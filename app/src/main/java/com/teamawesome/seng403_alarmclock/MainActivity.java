46a51e3afd581f11d6c14fab9b098438717dd396
amairJD <amairj965@gmail.com>
2017-01-29 23:32:47 -0700

    - initial commit

---
+++ app/src/main/java/com/teamawesome/seng403_alarmclock/MainActivity.java
@@ -0,0 +1,15 @@
+package com.teamawesome.seng403_alarmclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Initial Commit
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
