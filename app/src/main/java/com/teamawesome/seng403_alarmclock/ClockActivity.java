f28eaaeb399073d21cb7658f37ea8075b76689cd
spchampa <spcham@live.ca>
2017-03-18 20:46:15 -0600

    Merge Changes. Add Clock Swap Functionality to Master.
    # Conflicts:
    #	app/src/main/java/com/teamawesome/seng403_alarmclock/ClockActivity.java

--- app/src/main/java/com/teamawesome/seng403_alarmclock/ClockActivity.java
+++ app/src/main/java/com/teamawesome/seng403_alarmclock/ClockActivity.java
@@ -158,7 +158,6 @@ public class ClockActivity extends AppCompatActivity
 
             if (position == 0)
                 return new ClockFragment();
-            }
             else if (position == 1)
                 return new AlarmListFragment();
             else
@@ -184,6 +183,39 @@ public class ClockActivity extends AppCompatActivity
         }
     }
 
+    public void showAlert(String alarmTag, String alarmName) {
+        Intent myIntent = new Intent(this, DismissActivity.class);
+        myIntent.putExtra("ALARM_TAG", alarmTag);
+        myIntent.putExtra("ALARM_NAME", alarmName);
+        startActivityForResult(myIntent, 10);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10){
+            if(resultCode == RESULT_OK){
+                /**
+                 * Returning from DismissActivity after alarm was activated
+                 */
+                String alarmTag = data.getExtras().getString("ALARM_TAG");
+                int snoozeTime = data.getExtras().getInt("SNOOZE_TIME");
+
+                Fragment alarmFrag = getSupportFragmentManager().findFragmentByTag(alarmTag);
+
+                if (alarmFrag instanceof AlarmItem){
+                    AlarmItem currentAlarm = (AlarmItem)alarmFrag;
+                    if (snoozeTime != 0)
+                        currentAlarm.snoozeAlarm(snoozeTime);
+                    else
+                        currentAlarm.switchOff();
+                }
+
+
+            }
+        }
+
+    }
 
 
 
