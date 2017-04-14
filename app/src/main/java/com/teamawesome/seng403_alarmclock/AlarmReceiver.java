68a36fa1d7a0ef8fc6f56d0d0a7c1781bb5e05a3
Eric Matteucci <eric.matteucci@ucalgary.ca>
2017-04-13 22:38:18 -0600

    wanna pull

--- app/src/main/java/com/teamawesome/seng403_alarmclock/AlarmReceiver.java
+++ app/src/main/java/com/teamawesome/seng403_alarmclock/AlarmReceiver.java
@@ -13,22 +13,32 @@ import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
+    /** specifies which alarm has been triggered. Needed for the snoozing of an alarm, since the clock activity
+     *  does not know which has triggered.
+     */
     String alarmTag;
+
+    /**
+     * The label on this alarm, needed to be displayed on the dismiss alert window.
+     */
     String alarmName;
 
-
+    /**
+     * Defines what is to happen when the alarm is triggered.
+     * @param context
+     * @param intent Used to store passed information in the intent's bundle.
+     */
     @Override
     public void onReceive(Context context, Intent intent)
     {
+        // retrieve the alarmTag and alarmName from the intent to be passed to the AlarmCoordinator
         alarmTag = intent.getStringExtra("ALARM_TAG");
         alarmName = intent.getStringExtra("ALARM_NAME");
 
+        // create a toast to alert the user that the alarm has triggered
         Toast.makeText(context, "Alarm Activated", Toast.LENGTH_LONG).show();
 
-        // the intent should have the ringtone stored in it using a specific tag such as
-        // TAG_RINGTONE. This is a simple way to be able to have different ringtones for the
-        // alarms.
-
+        // send the information to the AlarmCoordinator, which will set the flow of events for the triggered alarm
         AlarmCoordinator.getInstance().activateAlarm(alarmTag, alarmName, context);
 
     }
